0f23379b6aa859ac237520aa5041e473e16291cf
David Dai <wdai9162@gmail.com>
2022-05-17 07:37:23 +1000

    update

---
+++ 1.Java/src/solutionpackage/findAPI.java
@@ -0,0 +1,14 @@
+package solutionpackage;
+
+import java.io.File; 
+import java.io.IOException;
+
+public class findAPI {
+    
+    public void createMyFile(String path, String name){
+
+    }
+
+
+
+}
